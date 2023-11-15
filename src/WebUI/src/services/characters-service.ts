9a09f60eacb6861feb513218998120580bc45b6d
namidaka <namidaka@DESKTOP-AHHV998>
2023-11-15 20:15:58 +0100

    mod : base speed 0.65 -> 0.6 , speed per athle 0.0273 -> 0.3

--- src/WebUI/src/services/characters-service.ts
+++ src/WebUI/src/services/characters-service.ts
@@ -250,7 +250,7 @@ export const computeSpeedStats = (
     1 / (1 + applyPolynomialFunction(strength - 3, weightReductionPolynomialFactor));
   const freeWeight = 2.5 * (1 + (strength - 3) / 30);
   const perceivedWeight = Math.max(totalEncumbrance - freeWeight, 0) * weightReductionFactor;
-  const nakedSpeed = 0.65 + 0.00105 * (20 * athletics + 2 * agility);
+  const nakedSpeed = 0.60 + 0.03 * (20 * athletics + 2 * agility) / 26.0;
   const currentSpeed = clamp(
     nakedSpeed * Math.pow(361 / (361 + Math.pow(perceivedWeight, 5)), 0.055),
     0.1,
