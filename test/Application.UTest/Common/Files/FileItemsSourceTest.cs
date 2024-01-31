ea4fd1ae072fe4447fb10aba5dbbb9ab7b03f7c7
Harry Gardner <93069911+Muparadzi@users.noreply.github.com>
2024-01-31 17:46:35 +0000

    mod: fix dtv archers (#261)
    * fix dtv bows
    
    * update unit test
    
    * remove debug output
    
    * compilation include
    
    * unit test refactor and test
    
    * restore typo

--- test/Application.UTest/Common/Files/FileItemsSourceTest.cs
+++ test/Application.UTest/Common/Files/FileItemsSourceTest.cs
@@ -143,22 +143,30 @@ public class FileItemsSourceTest
 
         string filepath = GetFilePath();
         string charactersFilePath = Path.Combine(filepath, "../../../../../src/Module.Server/ModuleData/dtv/dtv_characters.xml");
+        string dtvItemsFilePath = Path.Combine(filepath, "../../../../../src/Module.Server/ModuleData/dtv/dtv_weapons.xml");
         Console.WriteLine(filepath);
         string charactersXmlPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)
                                    + "/ModuleData/dtv/dtv_characters.xml";
         XDocument charactersDoc = XDocument.Load(charactersFilePath);
+        XDocument dtvItemsDoc = XDocument.Load(dtvItemsFilePath);
         string[] itemIdsFromXml = charactersDoc
             .Descendants("equipment")
             .Select(el => el.Attribute("id")!.Value["Item.".Length..])
             .ToArray();
+        var dtvItemIdsFromXml = dtvItemsDoc
+            .Descendants("Item")
+            .Select(el => el.Attribute("id")!.Value)
+            .ToHashSet();
+
+        var combinedItems = items.Concat(dtvItemIdsFromXml);
 
         Assert.Multiple(() =>
         {
             foreach (string itemId in itemIdsFromXml)
             {
-                if (!items.Contains(itemId))
+                if (!combinedItems.Contains(itemId))
                 {
-                    string closestItemId = TestHelper.FindClosestString(itemId, items);
+                    string closestItemId = TestHelper.FindClosestString(itemId, combinedItems);
                     Assert.Fail($"Character item {itemId} was not found in items.json. Did you mean {closestItemId}?");
                     charactersDoc
                     .Descendants("equipment")
