b555606c8f5a789b339fe81e64bb811d96cf5fa8
Harry Gardner <93069911+Muparadzi@users.noreply.github.com>
2024-03-02 13:28:09 +0000

    mod : add editor module (#286)

---
+++ src/Module.Editor/CrpgEditorSubModule.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.ModuleManager;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module;
+
+internal class CrpgEditorSubModule : MBSubModuleBase
+{
+    static CrpgEditorSubModule()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+            Debug.Print(args.ExceptionObject.ToString(), color: Debug.DebugColor.Red);
+    }
+
+    protected override void OnSubModuleLoad()
+    {
+        base.OnSubModuleLoad();
+    }
+
+    protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
+    {
+        base.InitializeGameStarter(game, starterObject);
+    }
+
+    protected override void OnApplicationTick(float delta)
+    {
+        base.OnApplicationTick(delta);
+    }
+
+}
