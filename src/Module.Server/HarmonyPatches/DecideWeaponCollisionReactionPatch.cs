03164b3d8ca3071858a4797f5d0504726ad14f4a
Harry Gardner <93069911+Muparadzi@users.noreply.github.com>
2025-02-11 00:15:25 +0100

    mod: add axes slicedthrough meleecollisionreaction (#483)

---
+++ src/Module.Server/HarmonyPatches/DecideWeaponCollisionReactionPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.HarmonyPatches;
+
+[HarmonyPatch]
+public class DecideWeaponCollisionReactionPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Mission), "DecideWeaponCollisionReaction")]
+    public static void AddAxeSlicedThrough(ref MeleeCollisionReaction colReaction, in MissionWeapon attackerWeapon, in AttackCollisionData collisionData)
+    {
+        // Check if the weapon used for the attack is an axe
+        var weaponClass = attackerWeapon.IsEmpty ? WeaponClass.Undefined : attackerWeapon.CurrentUsageItem.WeaponClass;
+        bool isAxe = weaponClass == WeaponClass.OneHandedAxe || weaponClass == WeaponClass.TwoHandedAxe;
+        // Check if the attack hit an enemy (not a shield or an obstacle)
+        bool hitEnemy = collisionData.IsColliderAgent && !collisionData.AttackBlockedWithShield;
+        // Modify the collision reaction if the conditions are met
+        if (isAxe && hitEnemy && colReaction != MeleeCollisionReaction.SlicedThrough)
+        {
+            colReaction = MeleeCollisionReaction.SlicedThrough;
+        }
+    }
+}
