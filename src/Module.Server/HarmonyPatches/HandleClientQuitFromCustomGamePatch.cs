16a3f924c2ac19c966c2446e18fc094b1d113e4d
Harry Gardner <93069911+Muparadzi@users.noreply.github.com>
2025-02-17 12:20:02 +0100

    fix: patch crash when more than one NetworkPeer found with VirtualPlayer.Id (#485)

---
+++ src/Module.Server/HarmonyPatches/HandleClientQuitFromCustomGamePatch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.DedicatedCustomServer;
+using TaleWorlds.PlayerServices;
+
+namespace Crpg.Module.HarmonyPatches;
+
+[HarmonyPatch]
+public class HandleClientQuitFromCustomGamePatch
+{
+#if CRPG_SERVER
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(DedicatedCustomGameServerHandler), "HandleClientQuitFromCustomGame")]
+    public static bool Prefix(PlayerId playerId)
+    {
+        Task.Run(async () =>
+        {
+            while (Mission.Current != null && Mission.Current.CurrentState != Mission.State.Continuing)
+            {
+                await Task.Delay(1);
+            }
+
+            NetworkCommunicator networkCommunicator = GameNetwork.NetworkPeers.FirstOrDefault((NetworkCommunicator x) => x.VirtualPlayer.Id == playerId);
+            if (networkCommunicator != null && !networkCommunicator.IsServerPeer)
+            {
+                networkCommunicator.QuitFromMission = true;
+                GameNetwork.AddNetworkPeerToDisconnectAsServer(networkCommunicator);
+                MBDebug.Print("player with id " + playerId.ToString() + " quit from game");
+            }
+        });
+
+        return false;
+    }
+#endif
+}
